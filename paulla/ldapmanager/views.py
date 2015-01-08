119a37edc22337dcc10167e002047cb9e0d94855
Simon RECHER, voileux <voileux@paulla.asso.fr>
2015-01-08 22:55:08 +0100

    add 'add' view

--- paulla/ldapmanager/views.py
+++ paulla/ldapmanager/views.py
@@ -1,6 +1,10 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
+@view_config(route_name='home', renderer='templates/home.pt')
+def view_home(request):
+    return {'project': 'paulla.ldapmanager'}
+
+@view_config(route_name='add', renderer='templates/add.pt')
+def view_add(request):
     return {'project': 'paulla.ldapmanager'}
