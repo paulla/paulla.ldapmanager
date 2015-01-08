ce42f8131869d35d26b7aad67ee51387f4c791ed
Simon RECHER, voileux <voileux@paulla.asso.fr>
2015-01-08 22:53:44 +0100

    add add route

--- paulla/ldapmanager/__init__.py
+++ paulla/ldapmanager/__init__.py
@@ -6,7 +6,21 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
+    config.include('pyramid_fanstatic')
+    config.include('rebecca.fanstatic')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('add', '/add')
+    config.add_fanstatic_resources(['js.bootstrap.bootstrap',
+                            	    'js.bootstrap.bootstrap_theme',
+                                     'css.fontawesome.fontawesome',
+                                    ], r'.*\.pt')
+
+
     config.scan()
     return config.make_wsgi_app()
+
+
+
+
+
