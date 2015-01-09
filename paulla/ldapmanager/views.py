e631cc70d4dd194dbf7e9bd277f202e0aada524c
Simon RECHER, voileux <voileux@paulla.asso.fr>
2015-01-09 15:48:30 +0100

    add get entries call's

--- paulla/ldapmanager/views.py
+++ paulla/ldapmanager/views.py
@@ -1,10 +1,14 @@
 from pyramid.view import view_config
-
+import utils
 
 @view_config(route_name='home', renderer='templates/home.pt')
 def view_home(request):
-    return {'project': 'paulla.ldapmanager'}
+    result = utils.find_entries("ou=users","(uid=*)")   
+    return {'project': 'paulla.ldapmanager' , 'users' : result[1] }
 
 @view_config(route_name='add', renderer='templates/add.pt')
 def view_add(request):
     return {'project': 'paulla.ldapmanager'}
+    
+
+# vim:set et sts=4 ts=4 tw=80:
