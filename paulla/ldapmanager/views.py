c8bce33d76c9ba634e3261716b0c4859d7e21334
Simon RECHER, voileux <voileux@paulla.asso.fr>
2015-01-09 20:46:32 +0100

    send good element to template if you want to work

--- paulla/ldapmanager/views.py
+++ paulla/ldapmanager/views.py
@@ -3,8 +3,8 @@ import utils
 
 @view_config(route_name='home', renderer='templates/home.pt')
 def view_home(request):
-    result = utils.find_entries("ou=users","(uid=*)")   
-    return {'project': 'paulla.ldapmanager' , 'users' : result[1] }
+    result = utils.find_entries("ou=users,","(uid=*)")   
+    return {'project': 'paulla.ldapmanager' , 'users' : result }
 
 @view_config(route_name='add', renderer='templates/add.pt')
 def view_add(request):
